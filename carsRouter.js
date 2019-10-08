46f2526ef0d80201667d72bc7d39a0fecb67dc8f
Roger McConkie <roger.mcconkiejr@gmail.com>
2019-10-08 15:28:51 -0600

    all endpoints working, MVP met

--- carsRouter.js
+++ carsRouter.js
@@ -15,5 +15,19 @@ router.get('/', (req, res)=>{
         res.status(500).json({message: 'Failed to retrieve data.'})
     })
 })
+router.post('/', (req, res) => {
+    const carData = req.body;
+    db('cars').insert(carData)
+    .then(ids => {
+      db('cars').where({ id: ids[0] })
+      .then(newCarEntry => {
+        res.status(201).json(newCarEntry);
+      });
+    })
+    .catch (err => {
+      console.log('POST error', err);
+      res.status(500).json({ message: "Failed to store data" });
+    });
+  });
 
 module.exports = router;
